1d3ec7449dd4aba3d0eb26ddc5748d1bc3b709c7
Jofr3 <jofre.scaricaciottoli@cirvianum.cat>
2021-11-17 15:24:26 +0100

    primer commit

--- script.js
+++ script.js
@@ -3,18 +3,21 @@ window.onload = function () {
 
     // Añadir las tres imágenes del directorio "img" al array IMAGENES.
     const IMAGENES = [];
+    IMAGENES[0] = 'img/img1.jpg';
+    IMAGENES[1] = 'img/img2.jpg';
+    IMAGENES[2] = 'img/img3.jpg';
 
-    const TIEMPO_INTERVALO_MILESIMAS_SEG = 1000;
+    const TIEMPO_INTERVALO_MILESIMAS_SEG = 500;
 
     // posición actual guarda el indice de la imágen que se está mostrando (del array IMAGENES)
     let posicionActual = 0;
 
     // variables con los elementos del DOM HTML, aplicar el selector necesario.
-    let $botonRetroceder
-    let $botonAvanzar 
-    let $imagen 
-    let $botonPlay 
-    let $botonStop
+    let $botonRetroceder = document.getElementById('retroceder');
+    let $botonAvanzar = document.getElementById('avanzar');
+    let $imagen = document.getElementById('imagen');
+    let $botonPlay = document.getElementById('play');
+    let $botonStop = document.getElementById('stop');
 
     // Identificador del proceso que se ejecuta con setInterval().
     let intervalo;
@@ -25,18 +28,49 @@ window.onload = function () {
      * Funcion que cambia la foto en la siguiente posicion
      */
     function pasarFoto() {
+        switch (posicionActual) {
+            case 0:
+                posicionActual++;
+                console.log(posicionActual);
+                break;
+            case 1:
+                posicionActual++;
+                console.log(posicionActual);
+                break;
+            case 2:
+                posicionActual = 0;
+                console.log(posicionActual);
+                break;
+        }
+
         // se incrementa el indice (posicionActual)
 
         // ...y se muestra la imagen que toca.
+        renderizarImagen();
     }
 
     /**
      * Funcion que cambia la foto en la anterior posicion
      */
     function retrocederFoto() {
+        switch (posicionActual) {
+            case 0:
+                posicionActual = 2;
+                console.log(posicionActual);
+                break;
+            case 1:
+                posicionActual = 0;
+                console.log(posicionActual);
+                break;
+            case 2:
+                posicionActual = 1;
+                console.log(posicionActual);
+                break;
+        }
         // se incrementa el indice (posicionActual)
 
         // ...y se muestra la imagen que toca.
+        renderizarImagen();
     }
 
     /**
@@ -50,9 +84,15 @@ window.onload = function () {
      * Activa el autoplay de la imagen
      */
     function playIntervalo() {
+        intervalo = setInterval(pasarFoto, TIEMPO_INTERVALO_MILESIMAS_SEG);
+        setInterval(intervalo);
+        $botonStop.disabled = false;
+        $botonPlay.disabled = true;
+        $botonRetroceder.disabled = true;
+        $botonAvanzar.disabled = true;
         // Documentación de la función setInterval: https://developer.mozilla.org/en-US/docs/Web/API/setInterval
         // Mediante la función setInterval() se ejecuta la función pasarFoto cada TIEMPO_INTERVALO_MILESIMAS_SEG.
-        
+
 
         // Desactivamos los botones de control necesarios. Utilizando setAttribute y removeAttribute.
 
@@ -62,14 +102,39 @@ window.onload = function () {
      * Para el autoplay de la imagen
      */
     function stopIntervalo() {
+        clearInterval(intervalo);
+        $botonStop.disabled = true;
+        $botonPlay.disabled = false;
+        $botonRetroceder.disabled = false;
+        $botonAvanzar.disabled = false;
         // Desactivar la ejecución de intervalo.
 
         // Activamos los botones de control. Utilizando setAttribute y removeAttribute.
     }
 
+    let zoom = 1;
+    const speed = 0.1;
+
+    document.addEventListener("wheel", function(e) {
+
+        if(e.deltaY > 0){
+            $imagen.style.transform = `scale(${zoom += speed})`;
+            console.log(zoom);
+        }else{
+            $imagen.style.transform = `scale(${zoom -= speed})`;
+            console.log(zoom);
+        }
+
+    });
+
+
+
     // Eventos
     // Añadimos los evenntos necesarios para cada boton. Mediante addEventListener.
-
+    $botonPlay.addEventListener("click", playIntervalo);
+    $botonStop.addEventListener("click", stopIntervalo);
+    $botonAvanzar.addEventListener("click", pasarFoto);
+    $botonRetroceder.addEventListener("click", retrocederFoto);
     // Iniciar
     renderizarImagen();
 } 
